d15b03bb3dfa3a59b8e8f8d9176e3daf319e8176
ksadlo <unity.ksadlo@gmail.com>
2024-03-11 14:09:11 +0100

    test

--- Index.php
+++ Index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Complete Choice</title>
+    <title>Complete Choice by Krystian und Nexx</title>
 </head>
 <body>
-    
+
 </body>
 </html>
