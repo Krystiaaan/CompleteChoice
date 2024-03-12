7e3c9c248370b83cb84299fb91f660758ecd0bab
stneajra <nerhan.ajradini@stud.h-da.de>
2024-03-12 13:39:10 +0100

    Nav Eingerichtet

---
+++ parts/nav/CatNav.php
@@ -0,0 +1,11 @@
+<?php
+echo <<< NAVHTML
+<nav>
+    <ul class="CatNavUl">
+        <li class="catNavLi"><a href="#">Hardware</a></li>
+        <li class="catNavLi"><a href="#">Elektro Artikel</a></li>   
+        <li class="catNavLi"><a href="#">Bücher</a></li>
+        <li class="catNavLi"><a href="#">Search</a></li>
+    </ul>
+</nav>
+NAVHTML;
---
+++ parts/nav/userNav.php
@@ -0,0 +1,10 @@
+<?php
+echo <<< NAVHTML
+<nav>
+    <ul class="userNavUl">
+        <li class="userNavLi"><a href="#">Compete Choice</a></li>
+        <li class="userNavLi"><a href="#">Text</a></li>   
+        <li class="userNavLi"><a href="#">User</a></li>
+    </ul>
+</nav>
+NAVHTML;
